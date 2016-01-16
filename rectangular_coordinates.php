2bc63edd1d093e30e481f652eb73511ab7e613e5
elaor <f33ela@gmail.com>
2016-01-16 21:45:51 +0100

    Added tile objects and changed constructor to get a coordinate parameter

--- rectangular_coordinates.php
+++ rectangular_coordinates.php
@@ -2,33 +2,42 @@
 include_once 'coordinates.php';
 
 class Directions {
-	public static $EAST = 0;
-	public static $NORTH = 1;
-	public static $WEST = 2;
-	public static $SOUTH = 3;
+	const NORTH = 0;
+	const EAST = 1;
+	const SOUTH = 2;
+	const WEST = 3;
 }
-class RectangularCoordinates extends coordinates {
 
+class RectangularCoordinates extends coordinates {
+	
 	private $x;
 	private $y;
 	
-	function __construct($x, $y) {
+	public function __construct($x, $y) {
 		$this->x = $x;
 		$this->y = $y;
 	}
 	
+	public function getX() {
+		return $this->x;
+	}
+	
+	public function getY() {
+		return $this->y;
+	}
+	
 	function getNeighbor($direction) {
 		switch($direction) {
-			case "north": 
+			case Directions::NORTH: 
 				return new RectangularCoordinates($this->x, $this->y + 1);
 				break;
-			case "east":
+			case Directions::EAST:
 				return new RectangularCoordinates($this->x + 1, $this->y);
 				break;
-			case "south":
+			case Directions::SOUTH:
 				return new RectangularCoordinates($this->x, $this->y - 1);
 				break;
-			case "west":
+			case Directions::WEST:
 				return new RectangularCoordinates($this->x - 1, $this->y);
 				break;
 			default:
@@ -42,6 +51,6 @@ class RectangularCoordinates extends coordinates {
 	}
 }
 
-$c0 = new RectangularCoordinates(4, 1);
-echo $c0;
+#$c0 = new RectangularCoordinates(4, 1);
+#echo $c0;
 
--- rectangular_grid.php
+++ rectangular_grid.php
@@ -1,29 +1,57 @@
 <?php
 include_once 'grid.php';
+include_once 'rectangular_coordinates.php';
+include_once 'tile.php';
 
 class RectangularGrid extends Grid {
 	
 	private $tiles;
+	private $maxCoordinate; 
 	
-	function __construct($x, $y) {
-		for ($i = 0; $i < $x; $i++) {
-			for ($j = 0; $j < $y; $j++) {
-				$this->tiles[$i][$j] = 0;
+	public function __construct($maxCoordinates) {
+		$this->maxCoordinate = $maxCoordinates;
+		if (!$this->isValid($maxCoordinates)) {
+			# TODO: Raise error
+		}
+		for ($i = 0; $i < $maxCoordinates->getX() + 1; $i++) {
+			for ($j = 0; $j < $maxCoordinates->getY() + 1; $j++) {
+				$this->tiles[$i][$j] = new Tile();
 			}
 		}
+		$this->directions = array(Directions::EAST, Directions::NORTH,
+				                  Directions::SOUTH, Directions::WEST);
 	}
 	
-	function __toString() {
+	public function getTile($coordinates) {
+		return $this->tiles[$coordinates->getX()][$coordinates->getY()];
+	}
+	
+	public function isValid($coordinates) {
+		$x = $coordinates->getX();
+		$y = $coordinates->getY();
+		if ($x <= 0 || $y <= 0) {
+			return false;
+		}
+		if ($x > $this->maxCoordinate->getX() || $y > $this->maxCoordinate->getY()) {
+			return false;
+		}
+		return true;
+	}
+	
+	public function __toString() {
+		$output = "";
 		foreach ($this->tiles as $a) {
 			foreach ($a as $tile) {
 				$output .= $tile . " ";
 			}
 			$output .= "\n";
 		}
+		
 		return $output;
 	}
 }
 
-
-$test = new RectangularGrid(10, 20);
+$c = new RectangularCoordinates(20, 10);
+$test = new RectangularGrid($c);
 echo $test;
+
